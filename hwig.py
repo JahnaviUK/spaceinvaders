68e85b034fbb505e8ea804f35864d9ab7655380b
JahnaviUK <preethibajjappa@gmail.com>
2025-10-29 16:56:57 +0530

    tkinter

--- hwig.py
+++ hwig.py
@@ -44,3 +44,53 @@ font = pygame.font.Font("freesansbold.ttf",32)
 textx = 10
 texty = 10
 over_font = pygame.font.Font("freesansbold.ttf",64)
+def show_score(x,y):
+    score = font.render("score:"+str(score_value),True,(255,255,255))
+    screen.blit(score,(x,y))
+def game_over_text():
+    over_text = over_font.render("game over",True(255,255,255))
+    screen.blit(over_font,(200,250))
+def player(x,y):
+    screen.blit(playerimg,(x,y))
+def enemy(x,y,i):
+    screen.blit(enemyimg[i],(x,y))
+def fire_bullet(x,y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletimg,(x+16,y+10))
+def iscollistion(enemyx,enemyy,bulletx,bullety):
+    distance = math.sqrt((enemyx - bulletx)**2+(enemyy - bullety)**2)
+    return distance<COLLISION_DISTANCE
+running = True
+while running :
+    screen.fill((0,0,0))
+    screen.blit(background,(0,0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerx_change = -5
+            if event.key == pygame.K_RIGHT:
+                playerx_change = 5
+            if event.key == pygame.K_SPACE and bullet_state == "ready":
+               bulletx = playerx
+               fire_bullet(bulletx,bullety)
+        if event.type == pygame.KEYUP and event.key in [pygame.K_LEFT,pygame.K_RIGHT]:
+            playerx_change = 0
+        playerx+=playerx_change
+        playerx = max(0,min(playerx,SCREEN_WIDTH - 64))
+        for i in range(num_of_enemies):
+            if enemyy[i]>340:
+                for j in range(num_of_enemies):
+                    enemyy[j]=2000
+                game_over_text()
+                break
+            enemyx[i]+=enemyx_change[i]
+            if enemyx[i]<=0 or enemyx[i]>=SCREEN_WIDTH - 64:
+                enemyx_change[i]*=-1
+                enemyy[i]+=enemyy_change[i]
+    
+    
+
+    
