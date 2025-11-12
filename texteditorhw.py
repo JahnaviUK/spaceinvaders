de5c9b081b84b95d4e90ee1735ef3eb6dfdf28a5
JahnaviUK <preethibajjappa@gmail.com>
2025-11-12 16:42:22 +0530

    hw

--- texteditorhw.py
+++ texteditorhw.py
@@ -1,12 +1,10 @@
 from tkinter import *
 from tkinter.filedialog import askopenfilename,asksaveasfilename
-#set up the window
 window = Tk()
 window.title("My Text Editor")
 window.geometry("600x500")
 window.rowconfigure(0,minsize = 800,weight = 1)
 window.columnconfigure(1,minsize=800,weight = 1)
-#function to open a file
 def openfile():
     filepath = askopenfilename(filetypes = [("Text Files","*.txt"),("All Files","*.*")])
     if not filepath :
